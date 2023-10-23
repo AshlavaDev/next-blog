5f99590c9884b29979f0b085ccf9c6a2ce209c8f
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-10-23 13:38:46 -0600

    add blog picture and alt text updater to update blog modal, swtich code to match update profile code

--- components/modals/UpdateBlogModal.tsx
+++ components/modals/UpdateBlogModal.tsx
@@ -9,47 +9,46 @@ import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 import { SafeBlog } from "@/types/type";
+import ImageUpload from "@/components/form/ImageUpload";
 
 interface IProps {
   onClose: () => void;
   blogData: SafeBlog | null;
 }
 
-//TODO: Add image upload and alt text updater
-
 export default function UpdateBlogModal({ onClose, blogData }: IProps) {
-  const [title, setTitle] = useState(blogData?.title);
-  const [description, setDescription] = useState(blogData?.description);
-  const [content, setContent] = useState(blogData?.content);
+  const [state, setState] = useState({
+    title: blogData?.title ?? "",
+    description: blogData?.description ?? "",
+    content: blogData?.content ?? "",
+    imageSrc: blogData?.imageSrc ?? "",
+    altText: blogData?.altText ?? "",
+  });
   const router = useRouter();
+  
+  function handleChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ): void {
+    const { id, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  }
 
-  function handleChange(e: ChangeEvent<HTMLTextAreaElement>, name: string) {
-    const { value } = e.target;
-    switch (name) {
-      case "title":
-        setTitle(value);
-        break;
-      case "description":
-        setDescription(value);
-        break;
-      case "content":
-        setContent(value);
-        break;
-      default:
-        break;
-    }
+  function setCustomValue(id: any, value: any) {
+    setState((prevValues) => ({
+      ...prevValues,
+      [id]: value,
+    }));
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    const updatedBlogData = {
-      title,
-      description,
-      content,
-    };
+    event.preventDefault();
 
     if (blogData) {
       axios
-        .put(`/api/blogs/${blogData.id}`, updatedBlogData)
+        .put(`/api/blogs/${blogData.id}`, state)
         .then(() => {
           toast.success("Blog updated!");
           router.refresh();
@@ -70,15 +69,16 @@ export default function UpdateBlogModal({ onClose, blogData }: IProps) {
     >
       <form
         onSubmit={handleSubmit}
-        className="w-full rounded-lg bg-white p-6 shadow-lg md:w-1/2"
+        className="w-full rounded-lg bg-white p-6 shadow-lg md:w-1/2 flex flex-col"
       >
         <h2 className="mb-4 text-lg font-medium">Update Blog</h2>
         <div className="mb-4">
           <label className="mb-2 block font-medium text-gray-700">Title</label>
           <textarea
             className="w-full rounded-lg border border-gray-400 p-2"
-            value={title}
-            onChange={(e) => handleChange(e, "title")}
+            id="title"
+            value={state.title}
+            onChange={handleChange}
           />
         </div>
         <div>
@@ -87,8 +87,9 @@ export default function UpdateBlogModal({ onClose, blogData }: IProps) {
           </label>
           <textarea
             className="w-full rounded-lg border border-gray-400 p-2"
-            value={description}
-            onChange={(e) => handleChange(e, "description")}
+            id="description"
+            value={state.description}
+            onChange={handleChange}
           />
         </div>
         <div className="mb-4">
@@ -97,8 +98,27 @@ export default function UpdateBlogModal({ onClose, blogData }: IProps) {
           </label>
           <textarea
             className="w-full rounded-lg border border-gray-400 p-2"
-            value={content}
-            onChange={(e) => handleChange(e, "content")}
+            id="content"
+            value={state.content}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex h-32 w-32 flex-col items-center justify-center self-center border border-black">
+          <ImageUpload
+            value={state.imageSrc}
+            onChange={(value) => setCustomValue("imageSrc", value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="mb-2 block font-medium text-gray-700">
+            Your Profile Image Alt Text
+          </label>
+          <input
+            className="w-full rounded-lg border border-gray-400 p-2"
+            type="text"
+            id="altText"
+            value={state.altText}
+            onChange={handleChange}
           />
         </div>
         <div className="flex justify-end gap-4">
